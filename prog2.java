145d73c12c05602cd245336125054026a093e362
mmerabi <merabimichael@gmail.com>
2018-09-23 16:47:21 -0700

    Add files via upload

--- prog2.java
+++ prog2.java
@@ -6,105 +6,100 @@
  * including finding height, balancing and inserting nodes.  */
 
 
-package prog2;
 
+//Getters and setters, constructors
 class StringAVLNode {
 	private String item;
 	private int balance;
-	private StringAVLNode left, right;
- 
+	private StringAVLNode left, right; 
  
 	public StringAVLNode(String str) {
-	 }
+		this.item=str;
+		this.left=null;
+		this.right=null;
+	 }	
 
 	public int getBalance() {
 		return balance;
-	 
 	}
 
 	public void setBalance(int bal) {
-	 
+	 this.balance=bal;
 	}
 
 	public String getItem() {
 		return item;
-	 
 	}
 
 	public StringAVLNode getLeft() {
 		return left;
-	 
 	}
 
 	public void setLeft(StringAVLNode pt) {
-	 
+	 this.left=pt;
 	}
+	
 	public StringAVLNode getRight() {
-		return left;
-	 
+		return left; 
 	}
 
 	public void setRight(StringAVLNode pt) {
-
+		this.right=pt;
 	}
-	
-}   //End of class
+}   //End of constructor class
+
+//===================================================================
 
 
 // StringAVLNode
-class StringAVLTree {
-
- // should really be private but I need access for my test program to work
-	
-	
- StringAVLNode root;
+class StringAVLTree {		
+	StringAVLNode root;
  
- // just one constructor
- 
- public StringAVLTree() {
-	 
- }
+	public StringAVLTree() {
+		this.root=null;
+	}
 
  // Rotate the node to the right
- private static StringAVLNode rotateRight(StringAVLNode t) {
-	return t;
-	 
- }
+	private static StringAVLNode rotateRight(StringAVLNode t) {
+		return t; 
+	}
+ 
 
  // Rotate the node to the left
- private static StringAVLNode rotateLeft(StringAVLNode t) {
-	return t;
-	 
- }
+	private static StringAVLNode rotateLeft(StringAVLNode t) {
+		return t;
+	}
+ 
 // For these next four, be sure not to use any global variables
  // Return the height of the tree – not to be used anywhere in insert or delete
- public int height() {
-	return 0;
-	 
- }
+	public int height() {
+		return 0; 
+	}
+ 
  // Return the number of leaves in the tree
- public int leafCt() {
-	return 0;
-	 
- }
+	public int leafCt() {
+		return 0;	 
+	}
+ 
  // Return the number of perfectly balanced AVL nodes
- public int balanced() {
-	return 0;
-	 
- }
+	public int balanced() {
+		return 0;	 
+	}
+ 
  // Return the inorder successor or null if there is none or str is not in the tree
- public String successor(String str) {
-	return str;
+	public String successor(String str) {
+		return str; 
+	}
+ 
+	public void insert(String str) {
 	 
- }
- public void insert(String str) {
-	 
- }
- private StringAVLNode insert(String str, StringAVLNode t) {
-	return t;
-	 
- }
- // who are you? Put your name here!
+	}
+	
+	private StringAVLNode insert(String str, StringAVLNode t) {
+		return t; 
+	}
+ 
+ 
 	 public static String myName() {
 		 return "Michael Merabi";
  		}
